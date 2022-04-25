be7f318f3f66290ba40d70263f07c0c8c90eafa6
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-04-25 02:26:32 +0100

    rafed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AbonementComponent } from './backoffice/abonement/abonement.component';
+import { AddabonementComponent } from './backoffice/addabonement/addabonement.component';
+import { HooomeComponent } from './backoffice/hooome/hooome.component';
+import { UpdateabonementComponent } from './backoffice/updateabonement/updateabonement.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+
+  {path: '', component: HooomeComponent},
+  {
+    path: 'home',
+    component: HooomeComponent,
+    children: [
+      {
+      path: 'abonement-management',
+      children: [
+      {path : 'abonement', component : AbonementComponent},
+      {path: 'addabonement', component : AddabonementComponent},
+      {path: 'updateabonement/:id', component: UpdateabonementComponent},
+    ]
+  },
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AbonementComponent } from './backoffice/abonement/abonement.component';
+import { AddabonementComponent } from './backoffice/addabonement/addabonement.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { UpdateabonementComponent } from './backoffice/updateabonement/updateabonement.component';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { SidebarComponent } from './backoffice/sidebar/sidebar.component';
+import { HeaderComponent } from './backoffice/header/header.component';
+import { FooterComponent } from './backoffice/footer/footer.component';
+import { HooomeComponent } from './backoffice/hooome/hooome.component';
+import { ContentComponent } from './content/content.component';
+import {AdminDashLayoutComponent} from './backoffice/admin-dash-layout/admin-dash-layout.component';
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbonementComponent,
+    AddabonementComponent,
+    UpdateabonementComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HooomeComponent,
+    ContentComponent,
+    AdminDashLayoutComponent,
+
+
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxQRCodeModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  })
 export class AppModule { }
